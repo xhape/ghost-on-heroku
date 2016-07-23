89b8bcb3929684238cd1c93e8f204a53ad2f7cb0
Angelo Javonitalla <angelo.javonitalla@gmail.com>
2016-07-23 09:59:48 +0800

    modify ghost to ideamarket

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var path = require('path');
-var ghost = require('ghost');
+var ideamarket = require('ideamarket');
 
-ghost({
+ideamarket({
   config: path.join(__dirname, 'config.js')
 }).then(function (ghostServer) {
   ghostServer.start();
