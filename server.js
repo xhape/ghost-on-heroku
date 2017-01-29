35289c5d1cf618c8113dc06d08e0490ae5268e9e
Angelo Javonitalla <amjavonitalla@gmail.com>
2017-01-29 10:17:19 +0800

    Updting config

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var path = require('path');
-var ideamarket = require('ideamarket');
+var ghost = require('ghost');
 
-ideamarket({
+ghost({
   config: path.join(__dirname, 'config.js')
 }).then(function (ghostServer) {
   ghostServer.start();
